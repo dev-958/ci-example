8a26cf32433c2dab4c885c2a73940715c2cbb998
dev-958 <wolfie.acb@gmail.com>
2019-08-09 12:35:30 +0100

    First Commit

---
+++ sub-module1/src/main/java/org/dev985/ciexample/submodule1/submodule1/CiExampleApplication.java
@@ -0,0 +1,13 @@
+package org.dev985.ciexample.submodule1.submodule1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiExampleApplication.class, args);
+	}
+
+}
---
+++ sub-module1/src/test/java/org/dev985/ciexample/submodule1/CiExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dev985.ciexample.submodule1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CiExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
